027c3146e4b5d5d4d7d4490a8e2c6a931ba8adc3
okitan <okitakunio@gmail.com>
2021-11-05 08:05:35 +0900

    :bulb: export definitions

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import type yargs from "yargs";
 
 export type ExtractArgumentType<T> = T extends yargs.Argv<infer U> ? U : never;
 
-export * from "./services/notion";
 export { commandModule as dbCommandModule } from "./commands/db";
+export * from "./definitions";
+export * from "./services/notion";
